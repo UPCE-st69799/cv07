3964e83803c1896c347c87e65b48c3865b04f5ad
Jan Korbel (st69799) <71786085+duffi4545@users.noreply.github.com>
2023-03-29 16:10:45 +0200

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import Task from "./component/Task";
+//import './App.css'
+import TaskCard from "./component/TaskCard";
+import Tasks from "./page/Tasks";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-        <Task></Task>
-
-
+        <Tasks></Tasks>
     </div>
   )
 }
 
-export default App
+export default App;
