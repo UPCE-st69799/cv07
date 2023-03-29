2adb61e989e52e2d51b23fe30cd047bf3b67e8c7
Jan Korbel (st69799) <71786085+duffi4545@users.noreply.github.com>
2023-03-29 14:55:08 +0200

    creating Task

---
+++ src/component/Task.tsx
@@ -0,0 +1,14 @@
+
+const Task = () => {
+    return <div>
+        <h1>Task 1</h1>
+        <p>popisek</p>
+        <span>datum vytvoreni</span>
+        <span>datum aktualizovani</span>
+        <br/>
+        <label>splněno</label>
+        <input type="checkbox" name="done"/>
+    </div>
+}
+
+export default Task;
