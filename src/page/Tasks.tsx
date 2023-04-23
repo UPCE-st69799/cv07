27c44d57af23f12b090e0175f88ccb5217ca740a
Jan Korbel (st69799) <71786085+duffi4545@users.noreply.github.com>
2023-04-23 23:16:46 +0200

    cv9

--- src/page/Tasks.tsx
+++ src/page/Tasks.tsx
@@ -1,15 +1,25 @@
 import TaskList from "../component/TaskList";
 import {Task} from "../data/init-data";
 import {useEffect, useState} from "react";
+import './Tasks.css';
+import {useSelector} from "react-redux";
+import {RootState} from "../features/store";
 
 const Tasks = () => {
+    const isLoggedIn = useSelector((state: RootState) => state.login.value);
+
     const [tasks, setTasks] = useState<Task[]>([]);
     const [error, setError] = useState<string | null>(null);
-    const [loading, setLoading] = useState<boolean | null>(true);
+    const [loading, setLoading] = useState<boolean | null>(false);
 
     useEffect(() => {
-        fetchData();
-    }, []);
+        console.log(`State changed in ${Tasks.name}: ${isLoggedIn}`);
+
+        if (isLoggedIn) {
+            setLoading(true);
+            fetchData();
+        }
+    }, [isLoggedIn]);
 
     const fetchData = async () => {
         const backendUrl = import.meta.env.VITE_BACKEND_URL;
@@ -29,7 +39,7 @@ const Tasks = () => {
         }
     };
 
-    return <div>
+    return <div className="tasks">
         {error && <div className="alert alert-danger">{error}</div>}
         {loading && <div className="alert alert-danger">loading</div>}
         <TaskList tasks={tasks} />
